cb6da4a2960ed0db6defcb5cfde1282ff9b13170
AnnJakubel <annabel.jakubel@gmail.com>
2023-01-28 22:51:31 +0200

    FindOdd exercise

--- src/main/java/ee/annjakubel/codewars/Main.java
+++ src/main/java/ee/annjakubel/codewars/Main.java
@@ -1,9 +1,13 @@
 package ee.annjakubel.codewars;
 
-import ee.annjakubel.codewars.oddevensort.OddEvenSort;
+import ee.annjakubel.codewars.findodd.FindOdd;
+
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to my completed Codewars tasks!");
+        FindOdd findOdd = new FindOdd();
+        int[] arr = {1,1,1,1,1,1,10,1,1,1,1};
+        System.out.println(findOdd.findIt(arr));
     }
 }
---
+++ src/main/java/ee/annjakubel/codewars/findodd/FindOdd.java
@@ -0,0 +1,31 @@
+package ee.annjakubel.codewars.findodd;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class FindOdd {
+    /*
+    Given an array of integers, find the one that appears an odd number of times.
+    There will always be only one integer that appears an odd number of times.
+    */
+    public FindOdd() {
+    }
+    public static int findIt(int[] a) {
+        int odd = -1;
+
+        for (int num : a) {
+            ArrayList<Integer> arrList = new ArrayList<>();
+
+            for (int i = 0; i < a.length; i++) {
+                if (num == a[i]) {
+                    arrList.add(a[i]);
+                }
+            }
+
+            if (arrList.size() % 2 != 0) {
+                odd = arrList.get(0);
+            }
+        }
+        return odd;
+    }
+}
