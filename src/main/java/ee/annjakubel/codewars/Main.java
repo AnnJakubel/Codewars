5783ed43aa88f2615475fcd97ddb3b477b426b77
AnnJakubel <annabel.jakubel@gmail.com>
2023-01-30 11:15:55 +0200

    CreatePhoneNumber exercise

--- src/main/java/ee/annjakubel/codewars/Main.java
+++ src/main/java/ee/annjakubel/codewars/Main.java
@@ -1,13 +1,11 @@
 package ee.annjakubel.codewars;
 
-import ee.annjakubel.codewars.findodd.FindOdd;
-
+import ee.annjakubel.codewars.createphonenumber.CreatePhoneNumber;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to my completed Codewars tasks!");
-        FindOdd findOdd = new FindOdd();
-        int[] arr = {1,1,1,1,1,1,10,1,1,1,1};
-        System.out.println(findOdd.findIt(arr));
+        CreatePhoneNumber createPhoneNumber = new CreatePhoneNumber();
+        System.out.println(createPhoneNumber.createPhoneNumber(new int[] {3, 7, 2, 5, 5, 5, 8, 6, 9, 1}));
     }
 }
---
+++ src/main/java/ee/annjakubel/codewars/createphonenumber/CreatePhoneNumber.java
@@ -0,0 +1,23 @@
+package ee.annjakubel.codewars.createphonenumber;
+
+public class CreatePhoneNumber {
+    public CreatePhoneNumber() {}
+
+    public static String createPhoneNumber(int[] numbers) {
+        StringBuilder firstPart = new StringBuilder();
+        StringBuilder secondPart = new StringBuilder();
+        StringBuilder thirdPart = new StringBuilder();
+
+        for (int i = 0; i < numbers.length; i ++) {
+            if (i <= 2) {
+                firstPart.append(numbers[i]);
+            } else if (i <= 5) {
+                secondPart.append(numbers[i]);
+            } else {
+                thirdPart.append(numbers[i]);
+            }
+        }
+
+        return "(" + firstPart.toString() + ") " + secondPart.toString() + "-" + thirdPart.toString();
+    }
+}
