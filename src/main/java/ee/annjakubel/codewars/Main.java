9cf95bc1b37e2c02e47dc5dd131e7e1644f59e9d
AnnJakubel <annabel.jakubel@gmail.com>
2023-02-01 23:53:23 +0200

    Removed unused imports on Main.java

--- src/main/java/ee/annjakubel/codewars/Main.java
+++ src/main/java/ee/annjakubel/codewars/Main.java
@@ -1,8 +1,5 @@
 package ee.annjakubel.codewars;
 
-import ee.annjakubel.codewars.createphonenumber.CreatePhoneNumber;
-import ee.annjakubel.codewars.smilefaces.SmileFaces;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to my completed Codewars tasks!");
